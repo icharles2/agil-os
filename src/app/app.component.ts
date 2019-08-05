17762ed7c1f9948d943dabceaf47618077a71e23
Alex <alex.r.otero12@gmail.com>
2019-08-05 18:46:43 -0500

    deleted server folder and created a couple of components/tamplates for the landing, home, and dTree pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+// import { homedir } from 'os';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html', //landing
+  // templateUrl: './homepage/home.component.html', //homepage
+  templateUrl: './dTree/priceRange.component.html',
+
+  // styleUrls: ['./app.component.css']
+  // styleUrls: ['../app/homepage/home.css']
 })
 export class AppComponent {
-  title = 'agil-os';
+  title = 'Plnnr';
 }
