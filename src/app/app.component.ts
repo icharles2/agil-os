356983fbef7813e21fa21cdca9f97cdf8355fef8
Alex <alex.r.otero12@gmail.com>
2019-08-06 14:31:33 -0500

    added two components for home page and start of dTree, will expand out to every view in the app inside of dTree folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html', //landing
+  templateUrl: './app.component.html', //landing
   // templateUrl: './homepage/home.component.html', //homepage
-  templateUrl: './dTree/priceRange.component.html',
+  // templateUrl: './dTree/priceRange.component.html', priceRange
 
   // styleUrls: ['./app.component.css']
   // styleUrls: ['../app/homepage/home.css']
 })
 export class AppComponent {
+  // also define dynamic values in the hmtl files like title= 'Pllnr'
   title = 'Plnnr';
 }
---
+++ src/app/dTree/priceRange.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // moduleId: module.id,
+  // selector used for when you are rendering a cpomponent and/or passing down properties to that component
+  selector: 'price-range',
+  templateUrl: '../dTree/priceRange.component.html',
+  // directives: []
+  // providers: [] where you specify the components you get stuff from *i think*
+})
+export class RangeComponent { 
+  constructor(/*get*/) {
+
+  }
+  // functions after constructor
+  ngOnInit() {
+
+  }
+
+}
