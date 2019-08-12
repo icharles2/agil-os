61bbf6b9c2bd7af738d5a0f2ef1870acd655af76
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-12 14:26:09 -0500

    Deletes earlier http test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -10,25 +10,19 @@ export class AppComponent {
   // also define dynamic values in the hmtl files like title= 'Pllnr'
   title = 'Plnnr';
   text = 'Click Me yuuuurd';
-  private nolaData = '../../../agil-server/sample_data/numbeo/newOrleans.js';
-  constructor(private http: HttpClient) {
+  
+  constructor() {
 
   }
   // functions after constructor
   ngOnInit() {
     // simple initialize func
-    console.log('App innnnnit');
-    console.log(this.getNolaData());
+   
   }
 
   countClicks() {
     console.log('button has been clicked yuuurd');
   }
 
-  getNolaData() {
-    return this.http.get(this.nolaData)
-    .subscribe((data) => {
-      console.log('request', data);
-    })
-  }
+  
 }
