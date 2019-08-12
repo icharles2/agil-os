b81686d8431c0af31d1b091b5f09c09fa9f0631e
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-12 14:00:18 -0500

    Begins table formatting

--- src/app/modules/pages/Dashboard/home.module.ts
+++ src/app/modules/pages/Dashboard/home.module.ts
@@ -1,21 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StylesModule } from 'src/app/shared/styles.module';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { TripComponent } from './trip/trip.component';
 import { HomeComponent } from './home.component'
 
+// import { AppModule } from 'src/app/app.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BudgetDetailComponent } from './budget-detail/budget-detail.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+// import { BudgetService } from '../../../../budget.service';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     TripComponent,
+    BudgetDetailComponent,
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    StylesModule
+    StylesModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
+  providers: [
+    // BudgetService,
   ]
 })
 export class HomeModule { }
