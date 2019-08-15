f53ce2d315996216604a4621538665ace6ae982e
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-14 23:39:24 -0500

    Creates models with types for trips and prices

---
+++ src/app/models/Prices.ts
@@ -0,0 +1,18 @@
+export class Price {
+  flight1?: number[]; // done
+  flight2?: number[]; // done
+  flight1Avg?: number;
+  flight2Avg?: number;
+  flightQ?: number; // done
+  meals?: number[]; // done
+  mealsQ?: number; // done
+  gas?: number; // done
+  gasTotal?: number; // done
+  rental?: number[]; // done
+  rentalQ?: number; // done
+  hotel?: number[]; // done
+  hotelQ?: number; // done
+  flightTotal?: number;
+  tripTotal?: number;
+  mealsTotal?: number; // done
+}
---
+++ src/app/models/Trips.ts
@@ -0,0 +1,14 @@
+export class Trip {
+  cityImg?: string;
+  title: string;
+  destination: string;
+  imgUrl?: string;
+  origin: string;
+  quality: number;
+  transpo: string;
+  lodging: string;
+  departure: string;
+  return: string;
+  rental?: boolean;
+  distance?: string;
+}
