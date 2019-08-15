1e7f85e3f4206ffd2562793616802af0dfde836f
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-15 15:49:33 -0500

    CSS changes

--- src/app/modules/pages/Dashboard/trip/trip.component.ts
+++ src/app/modules/pages/Dashboard/trip/trip.component.ts
@@ -30,8 +30,7 @@ export class TripComponent implements OnInit {
 
   getPricesTotal() {
     if (this.meals && this.transpo && this.lodging) {
-      this.total = this.prices['tripTotal'].reduce((a, b) => a + b);
-      Number(this.total.toFixed(2));
+      this.total = Number(this.prices['tripTotal'].reduce((a, b) => a + b).toFixed(2));
     }
   }
 
