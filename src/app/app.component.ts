bf935a9e072100ece3a4a01f32a71319c3dea96d
icharles2 <iacharles257@gmail.com>
2019-08-15 14:16:19 -0500

    smallchange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html', //landing 
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   // also define dynamic values in the hmtl files like title= 'Pllnr'
