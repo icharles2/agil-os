b1d026ee24d45ee7cc0af8d867a01c3cb4c6ae18
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-15 19:44:29 -0500

    Services fixed to get the correct trip length

--- src/app/shared/styles.module.ts
+++ src/app/shared/styles.module.ts
@@ -24,6 +24,7 @@ import {
   MatTableModule,
   MatProgressBarModule,
   MatTooltipModule,
+  MatButtonToggleModule,
 } from '@angular/material';
 
 @NgModule({
@@ -53,6 +54,7 @@ import {
     MatTableModule,
     MatProgressBarModule,
     MatTooltipModule,
+    MatButtonToggleModule,
   ],
   exports: [
     LayoutModule,
@@ -77,6 +79,7 @@ import {
     MatTableModule,
     MatProgressBarModule,
     MatTooltipModule,
+    MatButtonToggleModule,
   ],
 })
 export class StylesModule { }
