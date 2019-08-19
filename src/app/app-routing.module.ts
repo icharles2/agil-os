30d60612552883114479e3cfa55153df76fd41b8
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-19 13:04:03 -0500

    Changes Dashboard folder to be named Budget folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignComponent } from './modules/pages/Signup/signup.component';
-import { DashboardView } from './modules/pages/Dashboard/dashboard.component';
+import { DashboardView } from './modules/pages/Budget/dashboard.component';
 
 const routes: Routes = [
   {
@@ -15,11 +15,12 @@ const routes: Routes = [
   },
   {
     path: 'budget',
-    loadChildren: () => import('./modules/pages/Dashboard/dashboard.module').then(mod => mod.DashboardModule),
+    loadChildren: () => import('./modules/pages/Budget/dashboard.module').then(mod => mod.DashboardModule),
   },
   {
     path: 'landing',
     component: SignComponent,
+    // /modules/pages/Budget/home.module
   },
   {
     path: 'home',
--- src/app/modules/pages/New Trips/newTrip-routing.module.ts
+++ src/app/modules/pages/New Trips/newTrip-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewTripComponent } from './components/newTrip.component';
-import { DashboardView } from '../Dashboard/dashboard.component';
+import { DashboardView } from '../Budget/dashboard.component';
 
 const routes: Routes = [
   {
