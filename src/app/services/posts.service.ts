d2d3ff032b3bd6b294c47df8505a89749ff2c0c4
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-19 18:53:56 -0500

    Adds service for post functions to each table in the database

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -25,4 +25,26 @@ export class PostService {
       total: trip['total'],
     });
   }
+
+  savePrice(low, high, average, trips, quality, category, subTotal): Observable<Object> {
+    return this.http.post('http://localhost:3000/prices/create', {
+      low,
+      high,
+      average,
+      trips,
+      quality,
+      category,
+      subTotal,
+    });
+  }
+
+  saveCars(total, distance, price, tripId):Observable<Object> {
+    return this.http.post('http://localhost:3000/cars/create', {
+      total,
+      tripDistance: distance,
+      pricePerGal: price,
+      trips: tripId,
+    });
+  }
+
 }
