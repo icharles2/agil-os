4c37a27d39e1c7f0dec7296c2192e01dbb36c1ae
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-19 22:40:45 -0500

    Creates get request service to get trips by user and delete trip

---
+++ src/app/services/get.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { DBTrip } from '../models/DBTrips';
+@Injectable({
+  providedIn: 'root',
+})
+export class GetService {
+
+  constructor(private http: HttpClient) {}
+  url: string = 'http://localhost:3000/';
+
+  getTripsByUser(email) {
+    return this.http.get(`${this.url}trips/?email=${email}`);
+  }
+
+  deleteTrips(trip) {
+    return this.http.delete(`${this.url}trips/${trip.id}`);
+  }
+
+}
