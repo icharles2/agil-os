d62d5aec4a4b9d5376a9e35aa4830f614899acff
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-23 09:28:03 -0500

    Fixes stray forward slash

--- src/app/modules/pages/Budget/dashboard-routing.module.ts
+++ src/app/modules/pages/Budget/dashboard-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BudgetComponent } from './budgetBreakdown/budget.component';
 import { DashboardView } from './dashboard.component';
-/
+
 
 const routes: Routes = [
   {
