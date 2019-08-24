4994dcf0edc0e238b22ac9929689a053ad8928d6
lisaberteausmith <lisa.berteau.smith@gmail.com>
2019-08-24 13:48:35 -0500

    Removes unneeded breakpoint observers

--- src/app/shared/landing-nav/landing-nav.component.ts
+++ src/app/shared/landing-nav/landing-nav.component.ts
@@ -4,17 +4,12 @@ import { Observable } from 'rxjs';
 import { map, share } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'landing-nav',
   templateUrl: './landing-nav.component.html',
   styleUrls: ['./landing-nav.component.css'],
 })
 export class LandingNavComponent {
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(result => result.matches),
-    share(),
-  );
 
-  constructor(private breakpointObserver: BreakpointObserver, private router: Router) {}
+  constructor(private router: Router) {}
 }
